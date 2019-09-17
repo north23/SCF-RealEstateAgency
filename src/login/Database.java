8992388e5c08be4c479e6f989c977e6c897c221d
Antonio Jack <ict2jack@gmail.com>
2019-09-17 22:52:54 +0700

    Implement Login logic (not complete)

--- src/login/Database.java
+++ src/login/Database.java
@@ -1,44 +1,31 @@
 package login;
-import java.io.BufferedWriter;
-import java.io.File;
 
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import app.Customers;
+import exceptions.CorruptedFileException;
+
+import java.io.*;
+
+import java.util.*;
 
 
-public class Database 
-{
-    public static Map<String, User> load()
-    {
-        Map<String, User> user_map = new HashMap<String, User>();
-        try 
-        {
-            Scanner data_store = new Scanner(new File("fake-people-db.txt"));           
+public class Database {
+//    public static Map<String, User> load() {
+//        Map<String, User> user_map = new HashMap<String, User>();
+//        try {
+//            Scanner data_store = new Scanner(new File("fake-people-db.txt"));
+//
+//            while (data_store.hasNextLine()) {
+//                String[] split_string = data_store.nextLine().split(",");
+//                User u = new User(split_string);
+//                user_map.put(u.username, u);
+//            }
+//
+//        } catch (FileNotFoundException e) {
+//            System.out.println(e.getMessage());
+//        }
+//        return user_map;
+//    }
 
-            while (data_store.hasNextLine())
-            {
-                String[] split_string = data_store.nextLine().split(",");
-                User u = new User(split_string);
-                user_map.put(u.username, u);
-            }                   
-          
-        } 
-
-        catch (FileNotFoundException e) 
-        {
-            System.out.println(e.getMessage());
-        }
-        return user_map;
-    }
-    
     public void saveUser(User user) throws IOException {
         PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("fake-people-db.txt")));
 
@@ -48,8 +35,41 @@ public class Database
 
         pw.close();
     }
-    public static Map<String, User> create(){
-		return null;
-    	
+
+    public User loadUser(String userType, String username) throws IOException, CorruptedFileException {
+        String fileName;
+        if (userType.equals("customer")) {
+            fileName = "customer.txt";
+        } else {
+            fileName = "employee.txt";
+        }
+
+        // Create File Reading Objects
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        String line;
+
+        User user = null;
+        while ((line = br.readLine()) != null) {
+
+            user = createUser(line, userType);
+        }
+        if (br.readLine() == null)
+            throw new CorruptedFileException("The file is empty. No Booking data was loaded.");
+        br.close();
+        return user;
+    }
+
+    private User createUser(String line, String userType) {
+        StringTokenizer inReader = new StringTokenizer(line, ",");
+
+        String username = inReader.nextToken();
+        String password = inReader.nextToken();
+        String uniqueId = inReader.nextToken();
+
+        if (userType.equals("customer")) {
+            return new Customer();
+        } else {
+            return null;
+        }
     }
 }
--- src/login/User.java
+++ src/login/User.java
@@ -1,22 +1,31 @@
 package login;
 
 public class User {
-	
-	    public String first_name, last_name, email, suburb, username, password, uniqueId; 
 
-	    public User(String[] parts)
-	    {
-	        first_name = parts[0];
-	        last_name = parts[1];
-	        email = parts[2];
-	        suburb = parts[3];
-	        username = parts[4];
-	        password = parts[5];
-	        uniqueId = parts[6];
-	    }
-	    
-	    public boolean doesPasswordEqual(String password) {
-	        return this.password.equals(password);
-	    }
-	
+    public String first_name, last_name, email, suburb, username, password, uniqueId;
+
+    public User(String[] parts) {
+        first_name = parts[0];
+        last_name = parts[1];
+        email = parts[2];
+        suburb = parts[3];
+        username = parts[4];
+        password = parts[5];
+        uniqueId = parts[6];
+    }
+
+    public boolean doesPasswordEqual(String password) {
+        return this.password.equals(password);
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(username)
+                .append(",")
+                .append(password)
+                .append(",")
+				.append(uniqueId);
+
+        return sb.toString();
+    }
 }
