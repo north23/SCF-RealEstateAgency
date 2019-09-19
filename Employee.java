c8782b666151a58d73778c1f3ee5dfb183b370f7
Minjin chou <49186771+kuntakinte777@users.noreply.github.com>
2019-09-19 23:29:03 +1000

    updated a little bit of this for junyu

---
+++ Employee.java
@@ -0,0 +1,42 @@
+package app;
+
+import java.util.Scanner;
+
+public class Employee {
+	
+	private String empId;
+	
+	Scanner scanner = new Scanner (System.in);
+	
+	public int salary(int salary)
+	{
+		int salary1;
+		int hours;
+		//System.out.println("Please enter your base salary: ");
+		salary = scanner.nextInt();
+		System.out.println("Please enter the hours you have worked: ");
+		hours = scanner.nextInt();
+		
+		salary1 = salary * hours;
+		
+		return salary1;
+	}
+	
+	public Employee (String empId)
+	{
+		this.empId = empId;
+	}
+	
+	public void salesConsultant () {
+		
+	}
+	
+	public void propertyManagers ()
+	{
+		
+	}
+	public void branchAdministrator()
+	{
+		
+	}
+}
